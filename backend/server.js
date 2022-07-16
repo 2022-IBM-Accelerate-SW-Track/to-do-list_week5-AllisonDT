bf67d34147570ce4ef2f4d0bdc444bc48210c4e9
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2022-07-16 10:20:53 -0400

    week 5

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const fs = require("fs");
 
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
+console.log("printing");
 app.listen(port, () => console.log("Backend server live on " + port));
 
 app.get("/", (req, res) => {
@@ -44,12 +45,16 @@ app.get("/get/items", getItems)
 //** week5, get all items from the json database*/
   function getItems (request, response) {
     //begin here
-    
+    var data = fs.readFileSync('database.json');
+    response.json(JSON.parse(data));
   } 
 
 app.get("/get/searchitem",searchItems)
 //**week 5, search items service */
   function searchItems (request, response) {
     //begin here
-    
+    var searchField = request.query.taskname;
+    var json = JSON.parse (fs.readFileSync('database.json'));
+    var returnData = json.filter(jsondata => jsondata.Task === searchField);
+    response.json(returnData);
   }
